2d47083ee3778648e48668e144a0bc2c7338c76f
tmuzughi <55818129+tmuzughi@users.noreply.github.com>
2020-02-15 14:27:17 -0600

    added Makefile

--- 4280 Project 0/Files/node.h
+++ 4280 Project 0/Files/node.h
@@ -1,8 +1,10 @@
 #ifndef NODE_H
 #define NODE_H
 #define SIZE 100
+
 typedef struct node {
 	int stringLength;
 	char stringArray[SIZE][SIZE]; //note: I would like to change this dynamically to conserve memory
 } node;
+
 #endif
