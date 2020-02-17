2170404ce6838b3a9987474110eb1a49ea206b53
tmuzughi <55818129+tmuzughi@users.noreply.github.com>
2020-02-16 19:02:27 -0600

    finished input
    *Restructured code to match requirements
    - now checks keyboard or redirection first sending that data to output file
    - then does 'regular file input' as before from either given file or outputFile.txt
    *One bug remains (to resize end result array to match number of strings)
    - this is going in the nice to have section for now

--- 4280 Project 0/Files/P0.c
+++ 4280 Project 0/Files/P0.c
@@ -8,58 +8,75 @@
 
 #define SIZE 100
 
+void getKeyboardInput(int);
+
 int main(int argc, char *argv[]) {
-	if (argc == 2) {
-		/*-----------------------------------
-			Regular file input detected
-		-----------------------------------*/
-		FILE* inputFile;
-		char* mode = "r";
-		inputFile = fopen(argv[1], mode);
-		/*Error handling*/
-		if (inputFile == NULL) {
-			printf("Error reading file.\n");
-			exit(1);
-		}		
-		/*Determine the number of strings in the file*/
-		int numberOfStrings = 0;
-		char tempVariable[SIZE][SIZE];
-		while (fscanf(inputFile, "%s", tempVariable[0]) != EOF) numberOfStrings++;
-		printf("%d\n", numberOfStrings);
-		/*Close and reopen file*/
-		fclose(inputFile);
-		inputFile = fopen(argv[1], mode);
-		/*Put strings from file into array*/
-		char inputArray[SIZE][numberOfStrings];
-		int i = 0;
-		while (fscanf(inputFile, "%s", inputArray[i]) != EOF) i++;
-		fclose(inputFile);
-		/*Remove duplicates from array*/
-		char newArray[SIZE][numberOfStrings];
-		int index = 0;
-		int newSize = 0;
-		for (i = 0; i < numberOfStrings; i++) {
-			int j;
-			for (j = 0; j < i; j++) 
-				if (strcmp(inputArray[i], inputArray[j]) == 0)
-					break;
-			if (j == i) {
-				strcpy(newArray[index++], inputArray[i]);
-				newSize++;
-			}																
-		}
-	}
-	else {
-		/*------------------------------------
-		Keyboard input or redirecting detected
+	/*------------------------------------
+		Keyboard input or redirecting preparation
 		--------------------------------------*/
 		/*Error handling*/
-		if (argc > 2) {
-			printf("Error reading command line argument.\n");
-			exit(1);
+	if (argc > 2) {
+		printf("Error reading command line argument.\n");
+		exit(1);
+	}
+	getKeyboardInput(argc);
+
+	/*-----------------------------------
+			Regular file input section
+		-----------------------------------*/
+	FILE* inputFile;
+	char* mode = "r";
+	char outputFilename[] = "outputFile.txt";
+	if (argc == 2) 
+		inputFile = fopen(argv[1], mode);
+	else
+		inputFile = fopen(outputFilename, mode);
+	/*Error handling*/
+	if (inputFile == NULL) {
+		printf("Error reading file.\n");
+		exit(1);
+	}
+	/*Determine the number of strings in the file*/
+	int numberOfStrings = 0;
+	char tempVariable[SIZE][SIZE];
+	while (fscanf(inputFile, "%s", tempVariable[0]) != EOF)	numberOfStrings++;	
+	/*Close and reopen file*/
+	fclose(inputFile);
+	if (argc == 2) 
+		inputFile = fopen(argv[1], mode);
+	else
+		inputFile = fopen(outputFilename, mode);
+	/*Put strings from file into array*/
+	char inputArray[SIZE][SIZE];
+	int i = 0;
+	while (fscanf(inputFile, "%s", inputArray[i]) != EOF) i++;
+	fclose(inputFile);
+	/*Remove duplicates from array*/
+	char newArray[SIZE][SIZE];
+	int index = 0;
+	int newSize = 0;
+	for (i = 0; i < numberOfStrings; i++) {
+		int j;
+		for (j = 0; j < i; j++)
+			if (strcmp(inputArray[i], inputArray[j]) == 0)
+				break;
+		if (j == i) {
+			strcpy(newArray[index++], inputArray[i]);
+			newSize++;
 		}
-		/*Keyboard or redirecting?*/
-		if (isatty(STDIN_FILENO)) {
+	}
+	printf("\nThe strings are:\n");
+	for (i = 0; i < newSize; i++) {
+		printf("%s\n", &newArray[i]);
+	}
+
+	return 0;
+}
+
+void getKeyboardInput(int numberOfArguments) {
+	/*Keyboard or redirecting?*/
+	if (isatty(STDIN_FILENO)) {
+		if (numberOfArguments == 1) {
 			/*Take input from keyboard*/
 			FILE* outputFile;
 			char* mode = "w";
@@ -68,27 +85,18 @@ int main(int argc, char *argv[]) {
 			/*More error handling*/
 			if (outputFile == NULL) {
 				printf("Error creating temp file.");
-			}			
+			}
 			char holdIt[SIZE];
 			//prompt user for terminator
-			printf("Press 0 to stop.\n");
-			bool inputLoopTrigger = true;
-			int index = -1;
-			while (inputLoopTrigger) {
-				index++;
-				//get some input
-				scanf("%s", &holdIt);
-				//check for terminator
-				if (strchr(holdIt, '0'))
-					inputLoopTrigger = false;
-				if (inputLoopTrigger) {
-					//put that input into output file
-					fprintf(outputFile, "%s\n", holdIt);
-				}
-			}
-		}			
-		else
-		{
+			printf("Press 'CTRL + D' to stop.\n");
+			while (scanf("%s", holdIt) != EOF)
+				fprintf(outputFile, "%s\n", holdIt);
+			fclose(outputFile);
+		}
+	}
+	else
+	{
+		if (numberOfArguments != 2) {
 			/*Take input from redirected file*/
 			FILE* outputFile;
 			char* mode = "w";
@@ -106,5 +114,4 @@ int main(int argc, char *argv[]) {
 		}
 
 	}
-	return 0;
 }
--- 4280 Project 0/README.txt
+++ 4280 Project 0/README.txt
@@ -2,3 +2,8 @@
 *Created git hub folder for project 0
 *Created P0.c file
 *Worked on getting and storing input from keyboard
+
+2/15/20
+*Added regular file input
+*Added keyboard input
+*Added redirected input
