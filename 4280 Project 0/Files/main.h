a689ce99783be25d3b645e3a1bc07623c81c39a0
tmuzughi <55818129+tmuzughi@users.noreply.github.com>
2020-02-23 02:24:08 -0600

    done -testing/edge cases

---
+++ 4280 Project 0/Files/main.h
@@ -0,0 +1,11 @@
+#ifndef P0_H
+#define P0_H
+#define SIZE 100
+
+extern char globalArray[SIZE][SIZE];
+extern int newSize;
+extern int inOrderFlag;
+extern char baseName[SIZE];
+extern int numberOfArguments;
+
+#endif
--- 4280 Project 0/Files/tree.c
+++ 4280 Project 0/Files/tree.c
@@ -1,5 +1,5 @@
 #include "node.h"
-#include "P0.h"
+#include "main.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -8,174 +8,129 @@
 
 struct node root;
 
+
 void buildTree() {
 	/*Initalize the root node*/
 	
 	struct node* rootPtr = &root;
 	root.level = 0;
 	root.stringLength = strlen(globalArray[0]);
-	//strcpy(root.stringArray[0], globalArray[0]);
 	root.index = 0;
 	root.rightChild = NULL;
 	root.leftChild = NULL;
 	
-
-
-	/*
-	printf("Root is on level %d\n", root.level);
-	printf("First string in root is %s\n", root.stringArray[0]);
-	printf("String length is %d\n", root.stringLength);
-	*/
-
+	int currentLevel = 0;
 	int i;
 	struct node* currentNode = &root;
-	/*
-	printf("%d\n", currentNode->stringLength);
-	printf("***\n");
-	for (i = 0; i < newSize; i++) {
-		printf("%d\n", strlen(globalArray[i]));
-	}
-	*/
-
-	for (i = 0; i < newSize; i++) {
-		//printf("string length of current node at start of loop is %d\n", currentNode->stringLength);
+	
+	for (i = 0; i < newSize; i++) {		
 		/*Check current string length against node*/
 		if (strlen(globalArray[i]) == currentNode->stringLength) {
 			strcpy(&currentNode->stringArray[currentNode->index], globalArray[i]);
 			currentNode->index++;
+			currentNode->level = currentLevel;
 			/*Set currentNode back to root*/
 			currentNode = &root;
+			currentLevel = 0;
 		} 
 		else if (strlen(globalArray[i]) > currentNode->stringLength) {			
 			if (currentNode->rightChild == NULL) {
-				//printf("right child = null\n");
 				insertRight(currentNode, strlen(globalArray[i]));
 			}
 				currentNode = currentNode->rightChild;
+				currentLevel++;
 				i--;			
 		}
 		else
 		{
 			if (currentNode->leftChild == NULL) {
-				//printf("left child = null\n");
 				insertLeft(currentNode, strlen(globalArray[i]));
 			}
 			currentNode = currentNode->leftChild;
+			currentLevel++;
 			i--;
 		}
 		
-		/*Reset currentNode to root*/
-		//currentNode = &root;
 	}
-	/*
-	for (i = 0; i < 5; i++) {
-		printf("%s\n", root.stringArray[i]);
-	}
-	for (i = 0; i < 5; i++) {
-		currentNode = root.rightChild;
-		printf("%s\n", currentNode->stringArray[i]);
-	}
-	for (i = 0; i < 5; i++) {
-		currentNode = root.leftChild;
-		printf("%s\n", currentNode->stringArray[i]);
-	}
-	currentNode = root.rightChild;
-	if (currentNode->rightChild != NULL) {
-		currentNode = currentNode->rightChild;
-		for (i = 0; i < 5; i++) {
-			printf("%s\n", currentNode->stringArray[i]);
-		}
-	}
-	currentNode = root.leftChild;
-	if (currentNode->leftChild != NULL) {
-		currentNode = currentNode->leftChild;
-		for (i = 0; i < 5; i++) {
-			printf("%s\n", currentNode->stringArray[i]);
-		}
-	}
-	currentNode = root.leftChild;
-	if (currentNode->leftChild != NULL) {
-		currentNode = currentNode->leftChild;
-		if (currentNode->rightChild != NULL) {
-			currentNode = currentNode->rightChild;
-			for (i = 0; i < 5; i++) {
-				printf("%s\n", currentNode->stringArray[i]);
-			}
-		}
-		
-	}
-	*/
+	
 	
 }
 
-void printInorder(struct node* node)
+void printInorder(struct node* node, FILE* file)
 {
 	if (node == NULL)
 		return;
-
+	
 	/* first recur on left child */
-	printInorder(node->leftChild);
+	printInorder(node->leftChild, file);
 	int index = node->index;
 	int i;
+	if (node->level != 0)
+		fprintf(file, "%*c", (node->level * 2), ' ');
+	fprintf(file, "%d ", node->stringLength);
 	for (i = 0; i < index; i++)
-		printf("%s\n", node->stringArray[i]);
+		fprintf(file, "%s ", node->stringArray[i]);
+	fprintf(file, "\n");
 
 	/* now recur on right child */
-	printInorder(node->rightChild);
+	printInorder(node->rightChild, file);
 }
 
-void printPreorder(struct node* node)
+void printPreorder(struct node* node, FILE* file)
 {
 	if (node == NULL)
 		return;
+	char outputFilename[] = "outputFile.txt";
 
 	int index = node->index;
 	int i;
+	if (node->level != 0)
+		fprintf(file, "%*c", (node->level * 2), ' ');
+	fprintf(file, "%d ", node->stringLength);
 	for (i = 0; i < index; i++)
-		printf("%s\n", node->stringArray[i]);
+		fprintf(file, "%s ", node->stringArray[i]);
+	fprintf(file, "\n");
 
 	/* then recur on left sutree */
-	printPreorder(node->leftChild);
+	printPreorder(node->leftChild, file);
 
 	/* now recur on right subtree */
-	printPreorder(node->rightChild);
+	printPreorder(node->rightChild, file);
 }
 
-void printPostorder(struct node* node)
+void printPostorder(struct node* node, FILE* file)
 {
 	if (node == NULL)
 		return;
 
 	// first recur on left subtree 
-	printPostorder(node->leftChild);
+	printPostorder(node->leftChild, file);
 
 	// then recur on right subtree 
-	printPostorder(node->rightChild);
+	printPostorder(node->rightChild, file);
 
 	int index = node->index;
 	int i;
+	if (node->level != 0)
+		fprintf(file, "%*c", (node->level * 2), ' ');
+	fprintf(file, "%d ", node->stringLength);
 	for (i = 0; i < index; i++)
-		printf("%s\n", node->stringArray[i]);
+		fprintf(file, "%s ", node->stringArray[i]);
+	fprintf(file, "\n");
 }
 
 struct node* createNode(int stringLength) {
 	struct node* newNode = malloc(sizeof(struct node));
 	newNode->stringLength = stringLength;
-	//printf("string length of created node is %d\n", newNode->stringLength);
 	newNode->leftChild = NULL;
 	newNode->rightChild = NULL;
-	//printf("i created a node i think\n");
 	return newNode;
 }
 
 insertLeft(struct node* currentNode, int stringLength) {
-	//printf("string length of current string before insertion is %d\n", stringLength);
-	//printf("string length of passed node before insertion is %d\n", currentNode->stringLength);
 	currentNode->leftChild = createNode(stringLength);
 }
 
 insertRight(struct node* currentNode, int stringLength) {
-	//printf("string length of current string before insertion is %d\n", stringLength);
-	//printf("string length of passed node before insertion is %d\n", currentNode->stringLength);
 	currentNode->rightChild = createNode(stringLength);
 }
